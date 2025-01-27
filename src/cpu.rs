aed7571f5cf5c250e0e3a213ca163358ca37be9a
toshiyuki <toshiyuki.saito.16@gmail.com>
2025-01-27 09:33:13 +0900

    #010 汎用レジスタ実装。LUI, ORI命令実装。

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,16 +2,21 @@ use crate::interconnect::Interconnect;
 
 pub struct Cpu {
   pc: u32,
+  regs: [u32; 32],
   inter: Interconnect,
 }
 
 impl Cpu {
   pub fn new(inter: Interconnect) -> Self {
+    let mut regs = [0xDEAD_BEEF; 32];
+    regs[0] = 0;
     Self {
       pc: 0xBFC0_0000,
+      regs,
       inter
     }
   }
+
   pub fn run_next_instruction(&mut self) {
     let pc = self.pc;
     let instruction = self.load32(pc);
@@ -26,14 +31,33 @@ impl Cpu {
   fn decode_and_execute(&mut self, instruction: Instruction) {
     match instruction.function() {
       0b001111 => self.op_lui(instruction),
+      0b001101 => self.op_ori(instruction),
       _ => panic!("Unhandled instruction {:08X}", instruction.0),
     }
+  }
+
+  fn reg(&self, index: u32) -> u32 {
+    self.regs[index as usize]
+  }
+
+  fn set_reg(&mut self, index: u32, val: u32) {
+    self.regs[index as usize] = val;
+    self.regs[0] = 0;
   }
 
   fn op_lui(&mut self, instruction: Instruction) {
     let i = instruction.imm();
     let t = instruction.t();
-    panic!("what now?");
+    let v = i << 16;
+    self.set_reg(t, v);
+  }
+
+  fn op_ori(&mut self, instruction: Instruction) {
+    let i = instruction.imm();
+    let t = instruction.t();
+    let s = instruction.s();
+    let v = self.reg(s) | i;
+    self.set_reg(t, v);
   }
 }
 
