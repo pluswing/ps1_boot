1d2cc83ecd4e0c0a4e6d918d2bd9a31fcc9cf14c
pluswing <toshiyuki@pluswing.co.jp>
2025-01-28 10:14:24 +0900

    #011 SW命令実装中

--- src/cpu.rs
+++ src/cpu.rs
@@ -28,10 +28,15 @@ impl Cpu {
     self.inter.load32(addr)
   }
 
+  fn store32(&mut self, addr: u32, val: u32) {
+    self.inter.store32(addr, val)
+  }
+
   fn decode_and_execute(&mut self, instruction: Instruction) {
     match instruction.function() {
       0b001111 => self.op_lui(instruction),
       0b001101 => self.op_ori(instruction),
+      0b101011 => self.op_sw(instruction),
       _ => panic!("Unhandled instruction {:08X}", instruction.0),
     }
   }
@@ -59,6 +64,16 @@ impl Cpu {
     let v = self.reg(s) | i;
     self.set_reg(t, v);
   }
+
+  fn op_sw(&mut self, instruction: Instruction) {
+    let i = instruction.imm();
+    let t = instruction.t();
+    let s = instruction.s();
+
+    let addr = self.reg(s).wrapping_add(i);
+    let v = self.reg(t);
+    self.store32(addr, v);
+  }
 }
 
 struct Instruction(u32);
@@ -67,6 +82,11 @@ impl Instruction {
   fn function(&self) -> u32 {
     let Instruction(op) = self;
     op >> 26
+  }
+
+  fn s(&self) -> u32 {
+    let Instruction(op) = self;
+    (op >> 21) & 0x1F
   }
 
   fn t(&self) -> u32 {
--- src/interconnect.rs
+++ src/interconnect.rs
@@ -13,10 +13,20 @@ impl Interconnect {
   }
 
   pub fn load32(&self, addr: u32) -> u32 {
+    if addr % 4 != 0 {
+      panic!("Unalignd load32 address: {:08X}", addr);
+    }
     if let Some(offset) = map::BIOS.contains(addr) {
       return self.bios.load32(offset);
     }
     panic!("unhandled load32 at address {:08X}", addr);
+  }
+
+  pub fn store32(&mut self, addr: u32, val: u32) {
+    if addr % 4 != 0 {
+      panic!("Unalignd store32 address: {:08X}", addr);
+    }
+    panic!("unhandled store32 at address {:08X}", addr)
   }
 }
 
