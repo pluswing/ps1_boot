3f89aeac661250da5881df3a66e80c4a0caed4dd
pluswing <toshiyuki@pluswing.co.jp>
2025-05-14 10:07:00 +0900

    #071 ループ、ピッチ実装

--- src/spu.rs
+++ src/spu.rs
@@ -37,13 +37,34 @@ fn decode_adpcm_block(block: &[u8; 16], decoded: &mut [i16; 28], old_sample: &mu
 
 struct Voice {
   start_address: u32,
+  repeat_address: u32,
   current_address: u32,
   pitch_counter: u16,
   decode_buffer: [i16; 28],
   envelope: AdsrEnvelope,
+
+  sample_rete: u16,
+  current_buffer_idx: u8,
+  current_sample: i16,
 }
 
 impl Voice {
+  fn clock(&mut self, sound_ram: &[u8]) {
+    let pitch_counter_step = cmp::min(0x4000, self.sample_rete);
+    self.pitch_counter = self.pitch_counter + pitch_counter_step;
+
+    while self.pitch_counter >= 0x1000 {
+      self.pitch_counter = self.pitch_counter - 0x1000;
+      self.current_buffer_idx = self.current_buffer_idx + 1;
+
+      if self.current_buffer_idx == 28 {
+        self.current_buffer_idx = 0;
+        self.decode_next_block(sound_ram);
+      }
+    }
+    self.current_sample = self.decode_buffer[self.current_buffer_idx as usize];
+  }
+
   fn key_on(&mut self, sound_ram: &[u8]) {
     self.envelope.key_on();
 
@@ -58,6 +79,41 @@ impl Voice {
       block,
       &mut self.decode_buffer,
       &mut self.decode_buffer[self.decode_buffer.len() - 1],
-      &mut self.decode_buffer[self.decode_buffer.len() - 2])
+      &mut self.decode_buffer[self.decode_buffer.len() - 2]);
+
+      let loop_end = block[1] & (1 << 0) != 0;
+      let loop_repeat = block[1] & (1 << 1) != 0;
+      let loop_start = block[1] & (1 << 2) != 0;
+
+      if loop_start {
+        self.repeat_address = self.current_address;
+      }
+
+      if loop_end {
+        self.current_address = self.repeat_address;
+
+        if !loop_repeat {
+          self.envelope.volume = 0;
+          self.envelope.key_off();
+        }
+      } else {
+        self.current_address += 16;
+      }
+  }
+}
+
+struct AdsrEnvelope {
+  volume: u32,
+}
+
+impl AdsrEnvelope {
+  fn new() -> Self {
+    Self {
+      volume: 0
+    }
+  }
+  fn key_on(&self) {
+  }
+  fn key_off(&self) {
   }
 }
